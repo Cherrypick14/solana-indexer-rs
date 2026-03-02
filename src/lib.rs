421bb8199247ac4130742f78e314014837afee71
Cherrypick14 <cherrypicklet@gmail.com>
2026-03-02 20:49:49 +0300

    feat(lib): expose indexer module and exports

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,9 @@ pub mod database;
 pub mod error;
 pub mod models;
 pub mod utils;
+pub mod indexer;
 
 pub use error::{IndexerError, Result};
 pub use config::Config;
 pub use database::Database;
+pub use indexer::Indexer;
